56f3aed1ef5cdb187c93294bad2b0666449a6037
SuperDelphi <44942598+SuperDelphi@users.noreply.github.com>
2022-03-14 15:40:12 +0100

    Refactoring

--- js/index.js
+++ js/index.js
@@ -74,6 +74,7 @@ class Animator {
 }
 
 class Card extends PIXI.Container {
+    game;
     showingFront;
     faceContainer;
     front;
@@ -84,8 +85,9 @@ class Card extends PIXI.Container {
 
     RANDOM_ROTATION_ANGLE = Math.PI / 24;
 
-    constructor(name, front, back) {
+    constructor(game, name, front, back) {
         super();
+        this.game = game;
         this.name = name;
 
         this.front = front;
@@ -115,8 +117,8 @@ class Card extends PIXI.Container {
         this.buttonMode = "pointer";
 
         this.on("pointertap", () => { // Flip the card
-            if (!this.animating && selectedCards.length < 2 && !this.showingFront) { // If it isn't already animating
-                chooseCard(this);
+            if (!this.animating && this.game.selectedCards.length < 2 && !this.showingFront) { // If it isn't already animating
+                this.game.chooseCard(this);
             }
         });
         this.on("pointerover", () => { // Fired when the mouse enters card boundaries
@@ -146,187 +148,209 @@ class Card extends PIXI.Container {
     }
 }
 
-function chooseCard(card) {
-    if (selectedCards.length < 2) {
-        Animator.flip(card); // Animation
-        selectedCards.push(card);
+class Game extends PIXI.Application {
+    motifImages = [
+        "camembert.png",
+        "circle.png",
+        "crescent.png",
+        "heart.png",
+        "square.png",
+        "star.png"
+    ];
+    CARD_RATIO = 2.5 / 3.5; // Classical playing card ratio
+    COLUMN_AMOUNT = 4;
+    ROW_AMOUNT = Math.ceil(this.motifImages.length * 2 / this.COLUMN_AMOUNT);
+    GUTTER = 20; // In pixels
 
-        // If we have two cards ...
-        if (selectedCards.length === 2) {
-            attempts++;
-            attemptsText.innerText = attempts.toString();
+    _w;
+    _h;
 
-            if (selectedCards[0].hasSameMotif(selectedCards[1])) { // If it's the right pair...
-                selectedCards.forEach(card => {
-                    Animator.funkyRotate(card, PAIR_ROTATION_DELAY); // Delay to wait the flip animation (we can improve later)
-                });
-
-                pairsFound++;
-                foundPairsText.innerText = pairsFound.toString();
-                selectedCards = [];
-
-                if (pairsFound === cards.length / 2) { // If all pairs have been found...
-                    win();
-                }
-            } else { // If not...
-                selectedCards.forEach(card => {
-                    Animator.flip(card, PAIR_FLIP_DELAY, () => { // Animation
-                        selectedCards = [];
-                    });
-                });
-            }
-        }
-    }
-}
-
-function win() {
-    winBanner.style.display = "block";
-    playAgainBtn.style.display = "inline-block";
-}
-
-function createCards() {
-    const unshuffledCards = [];
-    const backTexture = PIXI.Texture.from("img/card_back.png");
-
-    // Create the cards with the front and back sprites
-    motifImages.forEach(motif => {
-        for (let i = 0; i < 2; i++) { // Two cards
-            const frontSprite = PIXI.Sprite.from("img/motifs/" + motif);
-            const backSprite = PIXI.Sprite.from(backTexture);
-            const card = new Card(motif, frontSprite, backSprite);
-
-            unshuffledCards.push(card);
-        }
-    });
-
-    // Shuffle the cards
-    const cardsLength = unshuffledCards.length;
-    for (let i = 0; i < cardsLength; i++) {
-        const randomCard = unshuffledCards.splice(Math.round(Math.random() * (unshuffledCards.length - 1)), 1)[0];
-        cards.push(randomCard);
-    }
-}
-
-function deleteCards() {
-    if (cards) {
-        cards.forEach(card => {
-            card.destroy({children: true});
-        });
-    }
-    cards = [];
-}
-
-function displayCards() {
-    // TODO: Improve later
-
-    for (let i = 0; i < ROW_AMOUNT; i++) { // ROWS
-        for (let j = 0; j < COLUMN_AMOUNT; j++) { // EACH CARD
-            const card = cards[i * COLUMN_AMOUNT + j];
-            const cardHeight = ((ROW_AMOUNT - 1) * GUTTER + 2 * V_PADDING - app.screen.height) / -ROW_AMOUNT;
-            const cardWidth = cardHeight * CARD_RATIO; // Card width
-            const H_PADDING = (COLUMN_AMOUNT * cardWidth + (COLUMN_AMOUNT - 1) * GUTTER - app.screen.width) / -2; // Calculate the horizontal padding
-            const cardX = H_PADDING + j * cardWidth + j * GUTTER + (cardWidth / 2); // + (cardWidth / 2) compensates front/back middle anchor
-            const cardY = V_PADDING + i * cardHeight + i * GUTTER + (cardHeight / 2); // + (cardHeight / 2) compensates front/back middle anchor
-
-            // Set the card position/dimensions
-            card.x = cardX;
-            card.y = cardY;
-            card.width = cardWidth;
-            card.height = cardHeight;
-
-            app.stage.addChild(card);
-        }
-    }
-
-    // Display the total amount of pairs
-    pairAmountText.innerText = (cards.length / 2).toString();
-}
-
-// Resize the renderer (responsive) on window resize
-window.addEventListener("resize", () => {
-    _w = window.innerWidth * 0.7;
-    _h = window.innerHeight;
-    app.renderer.resize(_w, _h);
-})
-
-const motifImages = [
-    "camembert.png",
-    "circle.png",
-    "crescent.png",
-    "heart.png",
-    "square.png",
-    "star.png"
-];
-const CARD_RATIO = 2.5 / 3.5; // Classical playing card ratio
-const COLUMN_AMOUNT = 4;
-const ROW_AMOUNT = Math.ceil(motifImages.length * 2 / COLUMN_AMOUNT);
-const GUTTER = 20; // In pixels
-
-// Get the canvas element
-const canvas = document.getElementById("canvas");
-
-// Get info elements
-const foundPairsText = document.getElementById("found-pairs");
-const pairAmountText = document.getElementById("pair-amount");
-const attemptsText = document.getElementById("attempts");
-const winBanner = document.getElementById("win-banner");
-const playAgainBtn = document.getElementById("play-again");
-
-let _w = window.innerWidth * 0.7;
-let _h = window.innerHeight;
-
-let V_PADDING; // Window vertical padding (will be updated through ticker because it's dynamic)
-let PAIR_FLIP_DELAY;
-let PAIR_ROTATION_DELAY;
-
-let cards;
-let selectedCards; // Cards chosen by the player
-let pairsFound;
-let attempts;
-
-const app = new PIXI.Application({
-    antialias: true,
-    view: canvas,
-    width: _w,
-    height: _h,
-    backgroundAlpha: 0,
-    autoDensity: true
-})
-
-// Play again
-playAgainBtn.addEventListener("click", () => {
-    initGame();
-})
-
-function initGame() {
-    V_PADDING = app.renderer.height * 0.15; // Window vertical padding (will be updated through ticker because it's dynamic)
     PAIR_FLIP_DELAY = 700;
     PAIR_ROTATION_DELAY = 340;
 
-    deleteCards(); // Delete cards
+    cards = [];
+    selectedCards = [];
 
-    selectedCards = []; // Cards chosen by the player
     pairsFound = 0;
     attempts = 0;
 
-    // Create cards
-    createCards();
+    // HTML elements
+    canvas;
+    foundPairsText = document.getElementById("found-pairs");
+    pairAmountText = document.getElementById("pair-amount");
+    attemptsText = document.getElementById("attempts");
+    winBanner = document.getElementById("win-banner");
+    playAgainBtn = document.getElementById("play-again");
 
-    foundPairsText.innerText = pairsFound.toString();
-    pairAmountText.innerText = (cards.length / 2).toString();
-    attemptsText.innerText = attempts.toString();
+    constructor() {
+        const canvas = document.getElementById("canvas");
+        const _w = window.innerWidth * 0.7;
+        const _h = window.innerHeight;
 
-    // Hide win banner and reset button
-    winBanner.style.display = "none";
-    playAgainBtn.style.display = "none";
+        // Create a PixiJS application
+        super({
+            antialias: true,
+            view: canvas,
+            width: _w,
+            height: _h,
+            backgroundAlpha: 0,
+            autoDensity: true
+        });
 
-    // Animate
-    app.ticker.add(delta => {
-        V_PADDING = app.renderer.height * 0.15;
-        displayCards();
+        this.canvas = canvas;
+        this._w = _w;
+        this._h = _h;
 
-        TWEEN.update(); // NEVER FORGET AGAIN
-    });
+        // Resize
+        window.addEventListener("resize", () => {
+            this._w = window.innerWidth * 0.7;
+            this._h = window.innerHeight;
+            this.renderer.resize(_w, _h);
+        });
+
+        // Play again button
+        this.playAgainBtn.addEventListener("click", () => {
+            this.init();
+        })
+    }
+
+    init() {
+        this.deleteCards();
+        this.createCards();
+
+        this.selectedCards = [];
+        this.pairsFound = 0;
+        this.attempts = 0;
+
+        // Hide win banner and reset button
+        this.winBanner.style.display = "none";
+        this.playAgainBtn.style.display = "none";
+
+        // Initialize display of game info
+        this.foundPairsText.innerText = this.pairsFound.toString();
+        this.pairAmountText.innerText = (this.cards.length / 2).toString();
+        this.attemptsText.innerText = this.attempts.toString();
+
+        // Display the total amount of pairs
+        this.pairAmountText.innerText = (this.cards.length / 2).toString();
+
+        this.ticker.remove(this._animate.bind(this));
+        this.ticker.add(this._animate.bind(this));
+    }
+
+    displayCards() {
+        // TODO: Improve later
+
+        for (let i = 0; i < this.ROW_AMOUNT; i++) { // ROWS
+            for (let j = 0; j < this.COLUMN_AMOUNT; j++) { // EACH CARD
+                const card = this.cards[i * this.COLUMN_AMOUNT + j];
+                const cardHeight = ((this.ROW_AMOUNT - 1) * this.GUTTER + 2 * this._getVerticalPadding() - this.screen.height) / -this.ROW_AMOUNT;
+                const cardWidth = cardHeight * this.CARD_RATIO; // Card width
+                const H_PADDING = (this.COLUMN_AMOUNT * cardWidth + (this.COLUMN_AMOUNT - 1) * this.GUTTER - this.screen.width) / -2; // Calculate the horizontal padding
+                const cardX = H_PADDING + j * cardWidth + j * this.GUTTER + (cardWidth / 2); // + (cardWidth / 2) compensates front/back middle anchor
+                const cardY = this._getVerticalPadding() + i * cardHeight + i * this.GUTTER + (cardHeight / 2); // + (cardHeight / 2) compensates front/back middle anchor
+
+                // Set the card position/dimensions
+                card.x = cardX;
+                card.y = cardY;
+                card.width = cardWidth;
+                card.height = cardHeight;
+
+                this.stage.addChild(card);
+            }
+        }
+    } // Called every frame
+
+    _getVerticalPadding() {
+        return this.renderer.height * 0.15;
+    }
+
+    _animate(delta) {
+        this.displayCards();
+        TWEEN.update();
+    }
+
+    createCards() {
+        const unshuffledCards = [];
+        const backTexture = PIXI.Texture.from("img/card_back.png");
+
+        // Create the cards with the front and back sprites
+        this.motifImages.forEach(motif => {
+            for (let i = 0; i < 2; i++) { // Two cards
+                const frontSprite = PIXI.Sprite.from("img/motifs/" + motif);
+                const backSprite = PIXI.Sprite.from(backTexture);
+                const card = new Card(this, motif, frontSprite, backSprite);
+
+                unshuffledCards.push(card);
+            }
+        });
+
+        // Shuffle the cards
+        const cardsLength = unshuffledCards.length;
+        for (let i = 0; i < cardsLength; i++) {
+            const randomCard = unshuffledCards.splice(Math.round(Math.random() * (unshuffledCards.length - 1)), 1)[0];
+            this.cards.push(randomCard);
+        }
+    }
+
+    deleteCards() {
+        if (this.cards) {
+            this.cards.forEach(card => {
+                card.destroy({children: true});
+            });
+        }
+        this.cards = [];
+    }
+
+    chooseCard(card) {
+        if (this.selectedCards.length < 2) {
+            // Play sound
+            const slideSound = new Audio("sounds/card_slide1.mp3");
+            slideSound.volume = 0.2;
+            slideSound.play();
+
+            Animator.flip(card); // Animation
+            this.selectedCards.push(card);
+
+            // If we have two cards ...
+            if (this.selectedCards.length === 2) {
+                this.attempts++;
+                this.attemptsText.innerText = this.attempts.toString();
+
+                if (this.selectedCards[0].hasSameMotif(this.selectedCards[1])) { // If it's the right pair...
+                    this.selectedCards.forEach(card => {
+                        Animator.funkyRotate(card, this.PAIR_ROTATION_DELAY); // Delay to wait the flip animation (we can improve later)
+                    });
+
+                    this.pairsFound++;
+                    this.foundPairsText.innerText = this.pairsFound.toString();
+                    this.selectedCards = [];
+
+                    if (this.pairsFound === this.cards.length / 2) { // If all pairs have been found...
+                        this.win();
+                    }
+                } else { // If not...
+                    setTimeout(() => {
+                        // Play sound
+                        const flipSound = new Audio("sounds/card_flip1.mp3");
+                        flipSound.play();
+                    }, this.PAIR_FLIP_DELAY - 150);
+
+                    this.selectedCards.forEach(card => {
+                        Animator.flip(card, this.PAIR_FLIP_DELAY, () => { // Animation
+                            this.selectedCards = [];
+                        });
+                    });
+                }
+            }
+        }
+    }
+
+    win() {
+        this.winBanner.style.display = "block";
+        this.playAgainBtn.style.display = "inline-block";
+    }
 }
 
-initGame(); // Initialize the game
+const game = new Game();
+game.init();
